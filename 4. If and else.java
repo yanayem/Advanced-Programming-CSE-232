ad87b8661e4b2dd0240103dfb49819876b999781
Yeasin Arafat Nayem <87275236+yanayem@users.noreply.github.com>
2025-12-19 22:00:48 +0600

    Implement even-odd check in Main class

---
+++ 4. If and else.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num = 22;
+
+        if(num % 2 == 0)
+        {
+            System.out.println("even");
+        }
+        else if(num % 2 != 0 )
+        {
+            System.out.println("odd");
+        }
+
+        else
+        {
+            System.out.println("Error");
+        }
+
+    }
+}

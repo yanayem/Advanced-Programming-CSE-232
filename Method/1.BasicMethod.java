106b41d84a4bf41f21fa6e34a31ca0280adf0d65
Yeasin Arafat Nayem <87275236+yanayem@users.noreply.github.com>
2025-12-24 00:51:44 +0600

    Add basic Java method to print greeting

---
+++ Method/1.BasicMethod.java
@@ -0,0 +1,10 @@
+public class Main {
+    static void mymethod(){
+        System.out.println("Hello, Coder!");
+
+    }
+    public static void main(String[] args) {
+        mymethod();
+        
+    }
+}

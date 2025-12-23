6272781d5006c028835953c7f026ab80209d3d55
Yeasin Arafat Nayem <87275236+yanayem@users.noreply.github.com>
2025-12-24 01:21:19 +0600

    Add parameter method to greet a user

---
+++ Method/2.PerameterMethod.java
@@ -0,0 +1,10 @@
+public class Main {
+    static void mymethod(String name){
+        System.out.println("Hello, " + name + "!");
+
+    }
+    public static void main(String[] args) {
+        mymethod("coder");
+        
+    }
+}
